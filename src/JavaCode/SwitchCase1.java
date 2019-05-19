34bc8669780143c4f2a56794cb893e9745903b53
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-19 02:02:41 -0400

    Switch case 1 commit

---
+++ src/JavaCode/SwitchCase1.java
@@ -0,0 +1,30 @@
+package JavaCode;
+
+public class SwitchCase1 {
+
+    public static void main(String[]args){
+        int test=20;
+        switch (test){
+
+            case 10:
+                System.out.println("test is greater then 10");
+                break;
+            case 12:
+                System.out.println("test is greater then 12");
+                break;
+            case 15:
+                System.out.println("test is greater then 15");
+                break;
+            case 20:
+                System.out.println("test is greater then 20");
+                break;
+            case 19:
+                System.out.println("test is greater then 18");
+                break;
+             default:
+                 System.out.println("test is greater then 19");
+        }
+
+    }
+
+}
