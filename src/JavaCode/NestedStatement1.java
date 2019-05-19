4897639e40ce3a5fe16dbc70d8eadfb61e8dbb34
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-19 01:48:33 -0400

    Hurayra document commit

---
+++ src/JavaCode/NestedStatement1.java
@@ -0,0 +1,21 @@
+package JavaCode;
+
+public class NestedStatement1 {
+
+    public static void main(String[]args){
+        int apple=20;
+
+        if(apple==20){
+
+
+            if(apple<30)
+                System.out.println("apple is less then 30");
+            if(apple<25)
+                System.out.println("apple is less then 25");
+            else
+                System.out.println("apple is less then 30");
+
+        }
+    }
+
+}
